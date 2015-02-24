edf3b07fb1c5dd9fe4b11d4c9aefbbefd26a46bf
Will Skora <skorasaurus@gmail.com>
2015-02-24 11:46:46 -0500

    update attribution, improve comments

--- assets/js/app.js
+++ assets/js/app.js
@@ -501,8 +501,8 @@ if (!L.Browser.touch) {
 
 
 // jQuery.LiveAddress("3548360835023561920");
-var htmlKey = "3548360835619230092";      // Put your HTML key here
-// var htmlKey = "3548360835023561920";      // Put your HTML key here
+var htmlKey = "3548360835619230092";  // github    // Put your HTML key here
+// var htmlKey = "3548360835023561920";  // local
 
 // var testRunnerVersion = "1.1.6";  // The version of this test runner page
 
